abd3a3786e0f1c0f29876d65e854750549cbaed5
Krzysztof Wrona <krzychuwr1@gmail.com>
2018-05-01 01:19:32 +0200

    update to bootstrap 4, layout changes, loading hardcoded posts

---
+++ BlazorWebsite/BlazorWebsite.Server/Controllers/BlogPostController.cs
@@ -0,0 +1,29 @@
+﻿using BlazorWebsite.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebsite.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class BlogPostController : Controller
+    {
+        [HttpGet("[action]")]
+        public IEnumerable<BlogPostSummary> Summaries()
+        {
+            return Enumerable.Range(1, 5).Select(index => new BlogPostSummary
+            {
+                Date = DateTime.Now.AddDays(index),
+                Content = 
+$@"<strong>Post content {index}</strong> <br/>
+More content More content More content More content
+More content More content More content More content<br/>
+More content More content More content More content
+More content...",
+                Title = $"Post title {index}"
+            });
+        }
+    }
+}
---
+++ BlazorWebsite/BlazorWebsite.Shared/BlogPostSummary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorWebsite.Shared
+{
+    public class BlogPostSummary
+    {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
